9ec53b64c0ed2ca7b1c39ad81c92f58d4ba15b03
ohad <ohad@venn.city>
2019-01-13 15:29:58 +0200

    Type correction

--- eventConsumerSNS.js
+++ eventConsumerSNS.js
@@ -2,7 +2,7 @@ const TOPIC_NAME = process.env.TOPIC_NAME;
 
 module.exports.eventConsumerSNS = (event, context, callback) => {
   // print out the event information on the console (so that we can see it in the CloudWatch logs)
-  console.log(`I'm triggered by "eventProducer" through the SQS Queue "${TOPIC_NAME}"`);
+  console.log(`I'm triggered by "eventProducer" through the SNS Topic "${TOPIC_NAME}"`);
   console.log(`event:\n${JSON.stringify(event, null, 2)}`);
   return callback(null, {
     event: event
