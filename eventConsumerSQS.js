4d2bb5533c19ff40f49c2479f99e6fa71bfb6672
ohad <ohad@venn.city>
2019-01-14 17:44:44 +0200

    Refactored and added more properties and doc to sls.yml

--- eventConsumerSQS.js
+++ eventConsumerSQS.js
@@ -4,6 +4,9 @@ module.exports.eventConsumerSQS = (event, context, callback) => {
   // print out the event information on the console (so that we can see it in the CloudWatch logs)
   console.log(`I'm triggered by "eventProducer" through the SQS Queue "${QUEUE_NAME}"`);
   console.log(`event:\n${JSON.stringify(event, null, 2)}`);
+  var body = event.Records[0].body;
+  console.log("text: ",JSON.parse(body).text);
+
   return callback(null, {
     event: event
   });
